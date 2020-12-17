7bc179c85131346453ad3cb82689c14786c0d35c
sistemasLuciano <sistemas@clinicapergamino.com.ar>
2020-12-17 12:25:13 -0300

    first commit

---
+++ src/test/java/com/clinicapergamino/OSAM/OsamApplicationTests.java
@@ -0,0 +1,13 @@
+package com.clinicapergamino.OSAM;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OsamApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
